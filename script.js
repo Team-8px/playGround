0ddc4cffc03b71ec148a4bbd21e04467937e43de
jae <goawmfhfl1@naver.com>
2021-12-31 13:40:48 +0900

    FIx: 컬러 체인지 버튼
    1. 중간에 색상이 안바뀌는 현상 수정

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const colorChangebtn = document.querySelector(".btn");
 const colorContainer = document.querySelector(".color-container");
 
+let colorBox = [
+  "var(--main-color--navy)",
+  "var(--main-color--black)",
+  "var(--main-color--beige)",
+  "var(--main-color--red)",
+];
+
 let counter = 0;
 
 colorChangebtn.addEventListener("click", e => {
-  let colorBox = ["green", "black", "red", "gold"];
   if (colorBox.length === counter) {
-    counter = 0;
+    counter = 1;
+    colorContainer.style.backgroundColor = colorBox[0];
   } else if (e.target.classList.value === "btn") {
     colorContainer.style.backgroundColor = colorBox[counter];
     counter += 1;
   }
-  console.log(counter);
+  console.log(`inside counter${counter}`);
 });
